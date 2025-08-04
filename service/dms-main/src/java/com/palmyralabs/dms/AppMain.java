5484a52543d5c66c71c11bfd692dce52a368ec59
Raja K <ksvraja@gmail.com>
2025-08-04 12:47:34 +0530

    java project added

---
+++ service/dms-main/src/java/com/palmyralabs/dms/AppMain.java
@@ -0,0 +1,25 @@
+package com.palmyralabs.dms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+import com.palmyralabs.palmyra.spring.PalmyraSpringConfiguration;
+
+@SpringBootApplication
+@Import(PalmyraSpringConfiguration.class)
+@ComponentScan(value = { "com.palmyralabs.dms", "com.palmyralabs.palmyra.enhancer"})
+@EnableJpaRepositories
+@EntityScan
+@EnableMethodSecurity
+public class AppMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppMain.class, args);
+	}
+}
