038d109c49551d1a081fbf322379687082dfda6c
Akila1-2 <karuppsamyakila@gmail.com>
2025-08-08 09:57:45 +0530

    policymodel

--- service/dms-policy/src/main/java/com/palmyralabs/dms/model/PolicyFileModel.java
+++ service/dms-policy/src/main/java/com/palmyralabs/dms/model/PolicyFileModel.java
@@ -1,6 +1,5 @@
 package com.palmyralabs.dms.model;
 
-import java.sql.Timestamp;
 
 import com.palmyralabs.palmyra.base.annotations.PalmyraField;
 import com.palmyralabs.palmyra.base.annotations.PalmyraType;
@@ -15,35 +14,23 @@ import lombok.Setter;
 public class PolicyFileModel {
 	@PalmyraField(keyField = true)
 	private Integer id;
-	
+
 	@PalmyraField(mandatory = Mandatory.ALL)
-	private Long policyId;
-	
+	private PolicyModel policyId;
+
 	@PalmyraField
 	private String fileName;
-	
+
 	@PalmyraField
 	private Long fileSize;
-	
+
 	@PalmyraField
 	private String fileType;
-	
+
 	@PalmyraField
 	private Long docketType;
-	
+
 	@PalmyraField
 	private String objectUrl;
-	
-	@PalmyraField
-	private String createdBy;
-	
-	@PalmyraField
-	private String lastUpdBy;
-	
-	@PalmyraField
-	private Timestamp createdOn;
-	
-	@PalmyraField
-	private Timestamp lastUpdOn;
 
 }
--- service/dms-policy/src/main/java/com/palmyralabs/dms/model/PolicyModel.java
+++ service/dms-policy/src/main/java/com/palmyralabs/dms/model/PolicyModel.java
@@ -1,8 +1,6 @@
 package com.palmyralabs.dms.model;
 
-import java.sql.Timestamp;
 import java.time.LocalDate;
-import java.util.Date;
 
 import com.palmyralabs.palmyra.base.annotations.PalmyraField;
 import com.palmyralabs.palmyra.base.annotations.PalmyraType;
@@ -17,71 +15,59 @@ import lombok.Setter;
 public class PolicyModel {
 	@PalmyraField(keyField = true)
 	private Integer id;
-	
+
 	@PalmyraField(mandatory = Mandatory.ALL)
 	private Integer policyNumber;
-	
+
 	@PalmyraField
 	private LocalDate policyDate;
-	
+
 	@PalmyraField
 	private String customerId;
-	
+
 	@PalmyraField
 	private String name;
-	
+
 	@PalmyraField
 	private LocalDate dob;
-	
+
 	@PalmyraField
 	private LocalDate doc;
-	
+
 	@PalmyraField
 	private Long divisionCode;
-	
+
 	@PalmyraField
 	private String branchCode;
-	
+
 	@PalmyraField
 	private String branchNumber;
-	
+
 	@PalmyraField
 	private String boxNumber;
-	
+
 	@PalmyraField
 	private Integer rmsStatus;
-	
+
 	@PalmyraField
 	private String uploadLabel;
-	
+
 	@PalmyraField
 	private Long field1;
-	
+
 	@PalmyraField
 	private String field2;
-	
+
 	@PalmyraField
 	private String field3;
-	
+
 	@PalmyraField
 	private String mobileNumber;
-	
+
 	@PalmyraField
 	private Integer policyStatus;
-	
+
 	@PalmyraField
 	private String region;
-	
-	@PalmyraField
-	private String createdBy;
-	
-	@PalmyraField
-	private String lastUpdBy;
-	
-	@PalmyraField
-	private Timestamp createdOn;
-	
-	@PalmyraField
-	private Timestamp lastUpdOn;
 
 }
