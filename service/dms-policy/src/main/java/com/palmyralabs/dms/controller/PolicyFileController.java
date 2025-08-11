2f2bdc067620c7309c2471eb53fd4486fa4a4abf
krishna00sk00 <tsisureshkrishna@gmail.com>
2025-08-11 16:46:08 +0530

    few changes

--- service/dms-policy/src/main/java/com/palmyralabs/dms/controller/PolicyFileController.java
+++ service/dms-policy/src/main/java/com/palmyralabs/dms/controller/PolicyFileController.java
@@ -3,12 +3,12 @@ package com.palmyralabs.dms.controller;
 import java.io.IOException;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -18,7 +18,7 @@ import com.palmyralabs.palmyra.filemgmt.spring.ResponseFileEmitter;
 
 import lombok.RequiredArgsConstructor;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 @RequestMapping(path = "${palmyra.servlet.prefix-path:#{'palmyra'}}/policy")
 public class PolicyFileController {
