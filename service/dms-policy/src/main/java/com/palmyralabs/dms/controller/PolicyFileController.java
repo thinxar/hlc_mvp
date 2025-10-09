5325ccd2ec3b99cd3bc5385b715233f58fc7cefa
krishna00sk00 <tsisureshkrishna@gmail.com>
2025-10-09 17:22:57 +0530

    create endorsement api added

--- service/dms-policy/src/main/java/com/palmyralabs/dms/controller/PolicyFileController.java
+++ service/dms-policy/src/main/java/com/palmyralabs/dms/controller/PolicyFileController.java
@@ -1,14 +1,19 @@
 package com.palmyralabs.dms.controller;
 
+import java.io.IOException;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.palmyralabs.dms.model.EndorsementRequest;
+import com.palmyralabs.dms.service.EndorsementService;
 import com.palmyralabs.dms.service.PolicyFileService;
 import com.palmyralabs.palmyra.filemgmt.spring.ResponseFileEmitter;
 
@@ -20,6 +25,7 @@ import lombok.RequiredArgsConstructor;
 public class PolicyFileController {
 
 	private final PolicyFileService policyService;
+	private final EndorsementService endorseService;
 
 	@GetMapping("/{policyId}/file/{fileId}")
 	public ResponseFileEmitter downloadFile(@PathVariable("policyId") Integer policyId,
@@ -30,8 +36,15 @@ public class PolicyFileController {
 	@PostMapping("/{policyId}/docketType/{docketTypeId}/file")
 	public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file,
 			@PathVariable("policyId") Integer policyId, @PathVariable("docketTypeId") Integer docketTypeId) {
-		String result = policyService.upload(file, policyId,docketTypeId);
+		String result = policyService.upload(file, policyId, docketTypeId);
 		return ResponseEntity.ok(result);
+	}
+
+	@PostMapping("/{policyId}/endorsement/{docketTypeId}")
+	public ResponseEntity<String> createEndorsement(@RequestBody EndorsementRequest request,
+			@PathVariable("policyId") Integer policyId, @PathVariable("docketTypeId") Integer docketTypeId)
+			throws IOException {
+		return ResponseEntity.ok(endorseService.createEndorsement(request,policyId,docketTypeId));
 	}
 
 }
---
+++ service/dms-policy/src/main/java/com/palmyralabs/dms/service/EndorsementService.java
@@ -0,0 +1,97 @@
+package com.palmyralabs.dms.service;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.palmyralabs.dms.model.EndorsementRequest;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class EndorsementService {
+
+	private final PolicyFileService policyFileService;
+
+	private final String inputDir = "/home/palmyra/mydev/hlc_mvp/service/dms-main/src/main/resources/Templates/";
+
+	private String inputExtension = ".txt";
+	private String outputExtension = ".html";
+
+	public String createEndorsement(EndorsementRequest request, Integer policyId, Integer docketTypeId)
+			throws IOException {
+
+		String endorsementSubtype = request.getEndorsementSubType();
+		if (endorsementSubtype == null || endorsementSubtype.isEmpty()) {
+			throw new IllegalArgumentException("endorsementSubType not found");
+		}
+		File templateFile = findTemplateFile(new File(inputDir), endorsementSubtype + inputExtension);
+		if (templateFile == null) {
+			throw new IllegalArgumentException("Template file not found for: " + endorsementSubtype);
+		}
+		String content = new String(Files.readAllBytes(templateFile.toPath()));
+		
+		Map<String, Object>lowerCaseMap = processJsonData(request.getFormData());
+		Pattern pattern = Pattern.compile("%%(\\w+)%%");
+		Matcher matcher = pattern.matcher(content);
+		StringBuffer sb = new StringBuffer();
+
+		while (matcher.find()) {
+			String placeholder = matcher.group(1).toLowerCase();
+			Object value = lowerCaseMap.getOrDefault(placeholder, "");
+			matcher.appendReplacement(sb, "<b>" + value + "</b>");
+		}
+		matcher.appendTail(sb);
+		String outputFileName = endorsementSubtype + outputExtension;
+		
+		byte[] fileContent = sb.toString().getBytes();
+		MultipartFile multipartFile = new MockMultipartFile(outputFileName, outputFileName, "text/html", fileContent);
+		policyFileService.upload(multipartFile, policyId, docketTypeId);
+		return "file uploaded successfully";
+	}
+
+	private File findTemplateFile(File dir, String fileName) {
+		if (!dir.exists() || !dir.isDirectory())
+			return null;
+		for (File file : dir.listFiles()) {
+			if (file.isDirectory()) {
+				File found = findTemplateFile(file, fileName);
+				if (found != null)
+					return found;
+			} else if (file.getName().equalsIgnoreCase(fileName)) {
+				return file;
+			}
+		}
+		return null;
+	}
+	private Map<String, Object> processJsonData(Object jsonObj) {
+		ObjectMapper mapper = new ObjectMapper();
+		Map<String, Object> jsonMap = mapper.convertValue(jsonObj, new TypeReference<Map<String, Object>>() {
+		});
+
+		Map<String, Object> lowerCaseMap = new HashMap<>();
+		for (Map.Entry<String, Object> entry : jsonMap.entrySet()) {
+			String key = entry.getKey().toLowerCase();
+			Object value = entry.getValue();
+
+			if (value == null || value.toString().trim().isEmpty()) {
+				lowerCaseMap.put(key, "--");
+			} else {
+				lowerCaseMap.put(key, value);
+			}
+		}
+		return lowerCaseMap;
+	}
+
+}
